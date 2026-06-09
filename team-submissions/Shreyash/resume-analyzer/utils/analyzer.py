6ef342a6a9f2e59e150f3574d4fe518bd87023ee
Prateeksha Bhat <prateebhat2004@gmail.com>
2026-06-09 15:09:51 +0530

    Fixed utils imports

--- team-submissions/Shreyash/resume-analyzer/utils/analyzer.py
+++ team-submissions/Shreyash/resume-analyzer/utils/analyzer.py
@@ -1,8 +1,14 @@
 import json
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+import spacy
 
+try:
+    nlp = spacy.load("en_core_web_sm")
+
+except:
+
+    nlp = spacy.blank("en")
 def load_skills(path):
     with open(path,"r") as f:
         data=json.load(f)
